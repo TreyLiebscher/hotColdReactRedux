e882dd1640b3d8ef51a949978a12a12451b24720
Trey Liebscher <wcliebsc@gmail.com>
2018-09-04 13:17:51 -0400

    formatting

--- src/components/guess-count.js
+++ src/components/guess-count.js
@@ -4,12 +4,12 @@ import {connect} from 'react-redux';
 import './guess-count.css';
 
 export function GuessCount(props) {
-    const isPlural = props.guessCount !== 1;
+    const isPlural = props.guessCount != 1;
     const guessNoun = isPlural ? 'guesses' : 'guess';
 
     return (
         <h2 id="guessCount">
-            You've made <span id="count">{props.guessCount}</span> {guessNoun}!
+            You've made <span id="count">{props.guessCount}</span> {guessNoun}
         </h2>
     );
 }
